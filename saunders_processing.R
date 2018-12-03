4e94b383dace3ad2349a0a557674620d8c4ba759
Derek Howard <derekhoward@gmail.com>
2018-12-03 11:49:12 -0500

    update processing of Saunders data to generate df with expression by cell class

--- saunders_processing.R
+++ saunders_processing.R
@@ -18,7 +18,7 @@ tissue_levels <- list("CB" = "Cerebellum", "FC" = "Frontal Cortex", "PC" = "Post
                       "STR" = "Striatum", "SN" = "Substantia Nigra", "TH" = "Thalamus")
 meta %<>% mutate(tissue_name = recode(meta$tissue, !!!tissue_levels)) 
 meta %<>% select(-tissue) 
-
+meta_subset <- meta %>% select(tissue_subcluster, class, tissue_name)
 #test <- expression[, names(expression) %in% meta$tissue_subcluster]
 #dim(test)
 
@@ -35,6 +35,8 @@ print(dim(tidy))
 #tidy %<>% group_by(gene_symbol, tissue_subcluster) %>% summarise(log1ExpZ2 = mean(log1ExpZ))
 #print(dim(tidy))
 
+
+
 saunders_ranks_matrix <- tidy %>% 
   group_by(tissue_subcluster) %>% 
   mutate(log1ExpZRank = rank(log1ExpZ)) %>% 
@@ -45,3 +47,28 @@ write_csv(saunders_ranks_matrix, here('data', 'processed', 'processed_saunders_r
 saveRDS(saunders_ranks_matrix, file = here('data', 'processed', 'saunders_ranks_matrix.RDS'))
 write_csv(meta, here('data', 'processed', 'metadata.csv'))
 saveRDS(meta, file = here('data', 'processed', 'meta.RDS'))
+
+# to create separate table for AUC/pvalues by class
+tidy <- left_join(tidy, meta_subset, by='tissue_subcluster')
+tidy_by_class <- tidy %>% 
+  group_by(class, gene_symbol) %>% 
+  summarise(exp = mean(expression)) %>% 
+  mutate(log1Exp=log(1+exp))
+  
+tidy_by_class %<>% 
+  group_by(gene_symbol) %>% 
+  mutate(log1ExpZ = (log1Exp - mean(log1Exp)) / sd(log1Exp))
+
+dim(tidy_by_class)
+tidy_by_class %<>% filter(!is.na(log1ExpZ))
+
+class_ranks_matrix <- tidy_by_class %>% 
+  group_by(class) %>% 
+  mutate(log1ExpZRank = rank(log1ExpZ)) %>% 
+  select(-exp, -log1Exp, -log1ExpZ) %>% 
+  spread(class, value = log1ExpZRank)
+
+dim(class_ranks_matrix)
+
+write_csv(class_ranks_matrix, here('data', 'processed', 'processed_saunders_classes_ranks.csv'))
+saveRDS(class_ranks_matrix, file = here('data', 'processed', 'saunders_classes_ranks_matrix.RDS'))
